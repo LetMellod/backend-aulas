d1df4cc3cf9b5f680c9ad68a9e0a2171bdd61f07
Leticia Melo <leticiamelodia@gmail.com>
2025-09-09 21:46:53 -0300

    Códigos da aula 06

---
+++ aulas/aula05/index.js
@@ -0,0 +1,39 @@
+// importa o framework
+const express = require("express");
+
+// importa middleware de terceiros
+const cors = require('cors');
+
+// importa middleware de rota
+const router = require('./routerTarefa');
+
+// cria uma instância da aplicação
+const app = express();
+
+// middleware embutido ou integrado
+app.use(express.json());
+// ?param1=valor&param2=valor2
+app.use(express.urlencoded({ extended: false}));
+
+// middleware de terceiros
+app.use(cors());
+
+// middleware de aplicativo
+app.use((req, res, next) => {
+    console.log("Passei aqui");
+    next();
+})
+
+app.use('/tarefas', router);
+
+// middleware de erro
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).send("Algo de errado não está certo!");
+});
+
+// inicia a aplicação
+app.listen(3000, ()=>{
+    console.log("App está ON!");
+});
+
