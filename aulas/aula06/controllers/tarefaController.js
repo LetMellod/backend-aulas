715603c4052223b6a203b83938025d1ac7465d6c
Leticia Melo <leticiamelodia@gmail.com>
2025-09-23 21:29:14 -0300

    criando tarefa controller e models

--- aulas/aula06/controllers/tarefaController.js
+++ aulas/aula06/controllers/tarefaController.js
@@ -1,16 +1,40 @@
-const tarefas = [];
+const model = require("../models/tarefaModel");
 
 const listarTarefas = (req, res) => {
-  res.json(tarefas);
+  res.json(model.listar());
 };
 
 const criarTarefa = (req, res) => {
-  const novaTarefa = {
-    ...req.body,
-    id: tarefas.length + 1,
-  };
-  tarefas.push(novaTarefa);
+    const novaTarefa = model.criar(req.body);
   res.status(201).json(novaTarefa);
 };
 
-module.exports = { listarTarefas, criarTarefa };
+const obterTarefa = (req, res) => {
+    const { id } = req.params;
+    const tarefaEncontrada = model.obter(id);
+    if (tarefaEncontrada) return res.json(tarefaEncontrada);
+    res.status(404).json({msg: "Tarefa não encontrada"});
+}
+
+const atualizarTarefa = (req, res) => {
+  const { id } = req.params;
+  const tarefaEncontrada = tarefas.find(item => item.id == id);
+  if (tarefaEncontrada) {
+  tarefaEncontrada.nome = req.body.nome;
+  tarefaEncontrada.concluida = req.body.concluida;
+  return res.json(tarefaEncontrada);
+  }
+  res.status(404).json({ msg: "Tarefa não encontrada" });
+}
+
+const deletarTarefa = (req, res) => {
+  const { id }= req.params;
+  const posicao = tarefas.findIndex(item => item.id == id);
+  if (posicao >=0) {
+    tarefas.splice(posicao, 1);
+    return res.status(204).end();
+  }
+  res.status(404).json({ msg: "Tarefa não encontrada"});
+}
+
+module.exports = { listarTarefas, criarTarefa, obterTarefa, atualizarTarefa, deletarTarefa };
